befd8f56424b6ef9abb68fa517f25d0d718cbc52
Cedrick Maranda <cedmaranda@gmail.com>
2024-10-29 20:29:37 -0400

    adresse.cs

---
+++ 420DA3_A24_Projet/Business/Domain/Adresse.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class Address
+{
+    
+    public const int AddresseeMaxLength = 64;
+    public const int CivicNumberMaxLength = 6;
+    public const int StreetMaxLength = 128;
+    public const int CityMaxLength = 64;
+    public const int StateMaxLength = 64;
+    public const int CountryMaxLength = 64;
+    public const int PostalCodeMaxLength = 12;
+
+    public int Id { get; set; }
+    public AddressType AddressType { get; set; }
+    public string Addressee { get; set; }
+    public string CivicNumber { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string PostalCode { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime? DateDeleted { get; set; }
+    public DateTime? DateModified { get; set; }
+
+    public virtual Warehouse? OwnerWarehouse { get; set; }
+    public virtual ShippingOrder? OwnerShipOrder { get; set; }
+
+    public Address(string addressee, string civicNumber, string street, string city, string state, string country, string postalCode, AddressType addressType)
+    {
+        Addressee = addressee;
+        CivicNumber = civicNumber;
+        Street = street;
+        City = city;
+        State = state;
+        Country = country;
+        PostalCode = postalCode;
+        AddressType = addressType;
+        DateCreated = DateTime.Now;
+    }
+
+    
+    public bool ValidateCivicNumber(string civicNumber) =>
+        !string.IsNullOrEmpty(civicNumber) && civicNumber.Length <= CivicNumberMaxLength;
+
+    public bool ValidateStreet(string street) =>
+        !string.IsNullOrEmpty(street) && street.Length <= StreetMaxLength;
+
+    public bool ValidateCity(string city) =>
+        !string.IsNullOrEmpty(city) && city.Length <= CityMaxLength;
+
+    public bool ValidateState(string state) =>
+        !string.IsNullOrEmpty(state) && state.Length <= StateMaxLength;
+
+    public bool ValidateCountry(string country) =>
+        !string.IsNullOrEmpty(country) && country.Length <= CountryMaxLength;
+
+    public bool ValidatePostalCode(string postalCode) =>
+        !string.IsNullOrEmpty(postalCode) && postalCode.Length <= PostalCodeMaxLength;
+
+    
+    public override string ToString()
+    {
+        return $"{Addressee}, {CivicNumber} {Street}, {City}, {State}, {Country}, {PostalCode}";
+    }
+}
---
+++ 420DA3_A24_Projet/Business/Domain/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+public class Client
+{
+   
+   public const int ClientNameMaxLength = 128;
+   public const int ContactFirstNameMaxLength = 32;
+   public const int ContactLastNameMaxLength = 32;
+   public const int ContactEmailMaxLength = 128;
+   public const int ContactTelephoneMaxLength = 15;
+
+        
+   public int Id { get; set; }
+   public string ClientName { get; set; }
+   public int WarehouseId { get; set; }
+   public string ContactFirstName { get; set; }
+   public string ContactLastName { get; set; }
+   public string ContactEmail { get; set; }
+   public string ContactTelephone { get; set; }
+   public DateTime DateCreated { get; set; }
+   public DateTime? DateModified { get; set; }
+   public DateTime? DateDeleted { get; set; }
+
+        
+   public virtual Warehouse AssignedWarehouse { get; set; }
+   public virtual List<Product> Products { get; set; } = new List<Product>();
+   public virtual List<ShippingOrder> ShippingOrders { get; set; } = new List<ShippingOrder>();
+
+        
+   public Client(string clientName, string contactFirstName, string contactLastName, string contactEmail, string contactTelephone, int warehouseId)
+   {
+      ClientName = clientName;
+      ContactFirstName = contactFirstName;
+      ContactLastName = contactLastName;
+      ContactEmail = contactEmail;
+      ContactTelephone = contactTelephone;
+      WarehouseId = warehouseId;
+      DateCreated = DateTime.Now;
+   }
+
+        
+    public bool ValidateClientName(string clientName) =>
+     !string.IsNullOrEmpty(clientName) && clientName.Length <= ClientNameMaxLength;
+
+    public bool ValidateContactFirstName(string firstName) =>
+     !string.IsNullOrEmpty(firstName) && firstName.Length <= ContactFirstNameMaxLength;
+
+    public bool ValidateContactLastName(string lastName) =>
+     !string.IsNullOrEmpty(lastName) && lastName.Length <= ContactLastNameMaxLength;
+
+    public bool ValidateContactEmail(string email) =>
+     !string.IsNullOrEmpty(email) && email.Length <= ContactEmailMaxLength;
+
+    public bool ValidateContactTelephone(string telephone) =>
+     !string.IsNullOrEmpty(telephone) && telephone.Length <= ContactTelephoneMaxLength;
+
+    public override string ToString()
+    {
+      return $"#{Id} - {ClientName}, Contact: {ContactFirstName} {ContactLastName}";
+    }
+    
+}
