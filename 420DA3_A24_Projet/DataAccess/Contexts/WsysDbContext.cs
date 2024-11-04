bc4629578de8330031bbfd1c38ac31b729512489
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-04 07:26:09 -0500

    context config User et Role

--- 420DA3_A24_Projet/DataAccess/Contexts/WsysDbContext.cs
+++ 420DA3_A24_Projet/DataAccess/Contexts/WsysDbContext.cs
@@ -9,7 +9,7 @@ internal class WsysDbContext : DbContext {
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
         base.OnConfiguring(optionsBuilder);
-        optionsBuilder
+        _ = optionsBuilder
             .UseSqlServer("") // TODO: ajouteer stringue de connectshion
             .UseLazyLoadingProxies();
     }
@@ -17,6 +17,154 @@ internal class WsysDbContext : DbContext {
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
 
+
+
+        #region USER
+
+        _ = modelBuilder.Entity<User>()
+            .ToTable(nameof(this.Users))
+            .HasKey(user => user.Id);
+
+        _ = modelBuilder.Entity<User>()
+            .HasIndex(user => user.Username)
+            .IsUnique(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.Id)
+            .HasColumnName("Id")
+            .HasColumnOrder(0)
+            .HasColumnType("int")
+            .UseIdentityColumn(1, 1);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.Username)
+            .HasColumnName("Username")
+            .HasColumnOrder(1)
+            .HasColumnType($"nvarchar({User.USERNAME_MAX_LENGTH})")
+            .HasMaxLength(User.USERNAME_MAX_LENGTH)
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.PasswordHash)
+            .HasColumnName("PasswordHash")
+            .HasColumnOrder(2)
+            .HasColumnType($"nvarchar({User.PASSWORDHASH_MAX_LENGTH})")
+            .HasMaxLength(User.PASSWORDHASH_MAX_LENGTH)
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.EmployeeWarehouseId)
+            .HasColumnName("EmployeeWarehouseId")
+            .HasColumnOrder(3)
+            .HasColumnType("int")
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.DateCreated)
+            .HasColumnName("DateCreated")
+            .HasColumnOrder(4)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .HasDefaultValueSql("GETDATE()")
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.DateModified)
+            .HasColumnName("DateModified")
+            .HasColumnOrder(5)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.DateDeleted)
+            .HasColumnName("DateDeleted")
+            .HasColumnOrder(6)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.RowVersion)
+            .HasColumnName("RowVersion")
+            .HasColumnOrder(7)
+            .IsRowVersion();
+
+        _ = modelBuilder.Entity<User>()
+            .HasMany(user => user.Roles)
+            .WithMany(role => role.Users);
+
+        #endregion
+
+        #region ROLE
+
+        _ = modelBuilder.Entity<Role>()
+            .ToTable(nameof(this.Roles))
+            .HasKey(role => role.Id);
+
+        _ = modelBuilder.Entity<Role>()
+            .HasIndex(role => role.Name)
+            .IsUnique(true);
+
+        _ = modelBuilder.Entity<Role>()
+            .Property(role => role.Id)
+            .HasColumnName("Id")
+            .HasColumnOrder(0)
+            .HasColumnType("int")
+            .UseIdentityColumn(1, 1);
+
+        _ = modelBuilder.Entity<Role>()
+            .Property(role => role.Name)
+            .HasColumnName("Name")
+            .HasColumnOrder(1)
+            .HasColumnType($"nvarchar({Role.NAME_MAX_LENGTH})")
+            .HasMaxLength(Role.NAME_MAX_LENGTH)
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<Role>()
+            .Property(role => role.Description)
+            .HasColumnName("Description")
+            .HasColumnOrder(2)
+            .HasColumnType($"nvarchar({Role.DESCRIPTION_MAX_LENGTH})")
+            .HasMaxLength(Role.DESCRIPTION_MAX_LENGTH)
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<Role>()
+            .Property(role => role.DateCreated)
+            .HasColumnName("DateCreated")
+            .HasColumnOrder(3)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .HasDefaultValueSql("GETDATE()")
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<Role>()
+            .Property(role => role.DateModified)
+            .HasColumnName("DateModified")
+            .HasColumnOrder(4)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<Role>()
+            .Property(role => role.DateDeleted)
+            .HasColumnName("DateDeleted")
+            .HasColumnOrder(5)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<Role>()
+            .Property(role => role.RowVersion)
+            .HasColumnName("RowVersion")
+            .HasColumnOrder(6)
+            .IsRowVersion();
+
+        _ = modelBuilder.Entity<Role>()
+            .HasMany(role => role.Users)
+            .WithMany(user => user.Roles);
+
+        #endregion
 
         // TODO: @WHOLE_TEAM configurez vos entités ici
 
