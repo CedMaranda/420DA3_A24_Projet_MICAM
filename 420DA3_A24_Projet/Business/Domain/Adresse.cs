1f02b599dff740054382b4828ee67493e64448c9
Cedrick Maranda <cedmaranda@gmail.com>
2024-11-12 18:37:56 -0500

    validation

--- 420DA3_A24_Projet/Business/Domain/Adresse.cs
+++ 420DA3_A24_Projet/Business/Domain/Adresse.cs
@@ -27,9 +27,9 @@ public class Adress
     public virtual Warehouse? OwnerWarehouse { get; set; }
     public virtual ShippingOrder? OwnerShipOrder { get; set; }
 
-    public Adress(string adressee, string civicNumber, string street, string city, string state, string country, string postalCode, AddressType addressType)
+    public Adress(string adresse, string civicNumber, string street, string city, string state, string country, string postalCode, AddressType addressType)
     {
-        Adressee = adressee;
+        Adresse = adresse;
         CivicNumber = civicNumber;
         Street = street;
         City = city;
@@ -62,6 +62,6 @@ public class Adress
     
     public override string ToString()
     {
-        return $"{Adressee}, {CivicNumber} {Street}, {City}, {State}, {Country}, {PostalCode}";
+        return $"{Adresse}, {CivicNumber} {Street}, {City}, {State}, {Country}, {PostalCode}";
     }
 }
---
+++ 420DA3_A24_Projet/Business/Services/AdresseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class AdresseService
+{
+	public AdresseService()
+	{
+  
+
+
+
+
+	}
+}
