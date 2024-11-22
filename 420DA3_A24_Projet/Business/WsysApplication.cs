c5a9a1b11cc7680a1a071acf716ce92a2a8a0150
Cedrick Maranda <cedmaranda@gmail.com>
2024-11-22 10:37:50 -0500

    creation de mes views et WsysApplication

---
+++ 420DA3_A24_Projet/Business/WsysApplication.cs
@@ -0,0 +1,36 @@
+﻿using _420DA3_A24_Projet.Business.Services;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business;
+internal class WsysApplication {
+
+    private WsysDbContext context;
+    public AdresseService AdresseService { get; private set; }
+    public ClientService ClientService { get; private set; }
+
+
+
+
+    public WsysApplication() {
+
+        this.context = new WsysDbContext();
+        this.AdresseService = new AdresseService();
+        this.ClientService = new ClientService();
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
