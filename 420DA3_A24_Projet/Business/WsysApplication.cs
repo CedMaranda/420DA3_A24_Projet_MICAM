0e396ef29deb95cfcf81e04b4d5baa3ef5f06b96
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-22 10:53:18 -0500

    ajout de services pour user, role et login à la classe application

--- 420DA3_A24_Projet/Business/WsysApplication.cs
+++ 420DA3_A24_Projet/Business/WsysApplication.cs
@@ -1,17 +1,16 @@
 ﻿using _420DA3_A24_Projet.Business.Services;
 using _420DA3_A24_Projet.DataAccess.Contexts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business;
 internal class WsysApplication {
 
     private WsysDbContext context;
+    public PasswordService PasswordService { get; private set; }
     public AdresseService AdresseService { get; private set; }
     public ClientService ClientService { get; private set; }
+    public UserService UserService { get; private set; }
+    public RoleService RoleService { get; private set; }
+    public LoginService LoginService { get; private set; }
 
 
 
@@ -19,10 +18,15 @@ internal class WsysApplication {
     public WsysApplication() {
 
         this.context = new WsysDbContext();
+        this.PasswordService = PasswordService.GetInstance();
         this.AdresseService = new AdresseService();
         this.ClientService = new ClientService();
+        this.UserService = new UserService(this, this.context);
+        this.RoleService = new RoleService(this, this.context);
 
 
+
+        this.LoginService = new LoginService(this);
 
     }
 
