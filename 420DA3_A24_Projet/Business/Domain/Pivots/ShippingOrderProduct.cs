db82068e2fd11e22169241bc255d8b1b2bba0abd
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-29 15:16:11 -0500

    ajout shipping/purchase orders stuff.

---
+++ 420DA3_A24_Projet/Business/Domain/Pivots/ShippingOrderProduct.cs
@@ -0,0 +1,75 @@
+﻿namespace _420DA3_A24_Projet.Business.Domain.Pivots;
+
+/// <summary>
+/// Classe représentant les associations entre <see cref="ShippingOrder"/> et <see cref="Product"/>."/>.
+/// Utilisé pour représenter les produits d'un ordre d'expédition et leur quantité.
+/// </summary>
+public class ShippingOrderProduct {
+
+    private int quantity;
+
+
+    public int ShippingOderId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity {
+        get { return this.quantity; }
+        set {
+            if (!ValidateQuantity(value)) {
+                throw new ArgumentOutOfRangeException("Quantity", "Quantity must be greater than or equal to 1.");
+            }
+            this.quantity = value;
+        }
+    }
+    public byte[] RowVersion { get; set; } = null!;
+
+    /// <summary>
+    /// L'ordre d'expédition associé.
+    /// </summary>
+    public virtual ShippingOrder ShippingOrder { get; set; } = null!;
+    public virtual Produit Product { get; set; } = null!;
+
+    /// <summary>
+    /// Constructeur orienté création manuelle
+    /// </summary>
+    /// <param name="shippingOrderId">L'identifiant de l'ordre d'expédition.</param>
+    /// <param name="productId">L'identifiant du produit.</param>
+    /// <param name="quantity">La quantité du produit associé à cet ordre d'expédition.</param>
+    public ShippingOrderProduct(int shippingOrderId, int productId, int quantity = 0) {
+        this.ShippingOderId = shippingOrderId;
+        this.ProductId = productId;
+        this.Quantity = quantity;
+    }
+
+    /// <summary>
+    /// Constructeur orienté entity framework.
+    /// </summary>
+    /// <param name="shippingOrderId">L'identifiant de l'ordre d'expédition.</param>
+    /// <param name="productId">L'identifiant du produit.</param>
+    /// <param name="quantity">La quantité du produit associé à cet ordre d'expédition.</param>
+    /// <param name="rowVersion">Le numéro de version anti-concurrence de l'entrée dans la base de donnée.</param>
+    protected ShippingOrderProduct(int shippingOrderId, int productId, int quantity, byte[] rowVersion)
+        : this(shippingOrderId, productId, quantity) {
+        this.RowVersion = rowVersion;
+    }
+
+    /// <summary>
+    /// Méthode de validation de la quantité du produit.
+    /// La quantité doit être supérieure ou égale à 1.
+    /// </summary>
+    /// <param name="quantity">La quantité du produit.</param>
+    /// <returns><see langword="true"/> si valide, <see langword="false"/> sinon.</returns>
+    public static bool ValidateQuantity(int quantity) {
+        return quantity >= 1;
+    }
+
+
+    /// <summary>
+    /// Override de la méthode <see cref="object.ToString"/> pour affichage des associations
+    /// produit-ordre d'expédition dans des ListBox ou ComboBox.
+    /// </summary>
+    /// <returns>Un string décrivant l'association produit-ordre d'expédition.</returns>
+    public override string ToString() {
+        return $"{this.Product.nomproduit} (Qty: {this.Quantity})";
+    }
+
+}
--- 420DA3_A24_Projet/Business/Domain/PurchaseOrder.cs
+++ 420DA3_A24_Projet/Business/Domain/PurchaseOrder.cs
@@ -1,16 +1,93 @@
 ﻿using Project_Utilities.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
+
+
+/// <summary>
+/// Classe représentant un ordre de restockage.
+/// </summary>
 public class PurchaseOrder {
 
+    private int quantity;
+
     public int Id { get; set; }
+    public PurchaseOrderStatusEnum Status { get; set; }
+    public int Quantity {
+        get { return this.quantity; }
+        set {
+            if (!this.ValidateQuantity(value)) {
+                throw new ArgumentOutOfRangeException("Quantity", "Quantity must be greater than or equal to 1.");
+            }
+            this.quantity = value;
+        }
+    }
     public int OrderedProductId { get; set; }
     public int DestinationWarehouseId { get; set; }
-    public Product OrderedProduct { get; set; } = null!;
-    public Warehouse DestinationWarehouse { get; set; } = null!;
+    public DateTime? CompletionDate { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime? DateModified { get; set; }
+    public DateTime? DateDeleted { get; set; }
+    public byte[] RowVersion { get; set; } = null!;
+
+
+    public virtual Product OrderedProduct { get; set; } = null!;
+    public virtual Warehouse DestinationWarehouse { get; set; } = null!;
+
+
+    /// <summary>
+    /// Constructeur orienté création manuelle.
+    /// NOTE: Statut par défaut: "New".
+    /// </summary>
+    /// <param name="orderedProductId">L'identifiant du produit de l'ordre de restockage.</param>
+    /// <param name="destinationWarehouseId">L'identifiant de l'entrepôt de destination de l'ordre de restockage.</param>
+    /// <param name="quantity">La quantité du produit de l'ordre de restockage.</param>
+    public PurchaseOrder(int orderedProductId, int destinationWarehouseId, int quantity) {
+        this.OrderedProductId = orderedProductId;
+        this.DestinationWarehouseId = destinationWarehouseId;
+        this.Quantity = quantity;
+        this.Status = PurchaseOrderStatusEnum.New;
+    }
+
+
+    /// <summary>
+    /// Constructeur orienté entity framework.
+    /// </summary>
+    /// <param name="id">L'identifiant de l'ordre de restockage.</param>
+    /// <param name="status">Le statut de l'ordre de restockage.</param>
+    /// <param name="orderedProductId">L'identifiant du produit de l'ordre de restockage.</param>
+    /// <param name="destinationWarehouseId">L'identifiant de l'entrepôt de destination de l'ordre de restockage.</param>
+    /// <param name="quantity">La quantité du produit de l'ordre de restockage.</param>
+    /// <param name="completionDate">La date de complétion (réception par l'entrepôt) de l'ordre de restockage.</param>
+    /// <param name="dateCreated">La date de création de l'ordre de restockage.</param>
+    /// <param name="dateModified">La date de dernière modification de l'ordre de restockage.</param>
+    /// <param name="dateDeleted">La date de suppression de l'ordre de restockage.</param>
+    /// <param name="rowVersion">Le numéro de version anti-concurrence de l'entrée dans la base de donnée.</param>
+    protected PurchaseOrder(int id,
+        PurchaseOrderStatusEnum status,
+        int orderedProductId,
+        int destinationWarehouseId,
+        int quantity,
+        DateTime? completionDate,
+        DateTime dateCreated,
+        DateTime? dateModified,
+        DateTime? dateDeleted,
+        byte[] rowVersion)
+        : this(orderedProductId, destinationWarehouseId, quantity) {
+
+        this.Id = id;
+        this.Status = status;
+        this.CompletionDate = completionDate;
+        this.DateCreated = dateCreated;
+        this.DateModified = dateModified;
+        this.DateDeleted = dateDeleted;
+        this.RowVersion = rowVersion;
+    }
+
+    public bool ValidateQuantity(int quantity) {
+        return quantity >= 1;
+    }
+
+    public override string ToString() {
+        return $"#{this.Id} ({this.Status}) - {this.Quantity} x {this.OrderedProduct.nomproduit} - Destination: {this.DestinationWarehouse.nomEntrepot}";
+    }
 }
